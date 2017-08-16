a0a0bc498d1799f4b0b67d866222bc3afce91078
Helio Loureiro <helio.loureiro@ericsson.com>
2017-08-16 19:07:53 +0200

    Trying to upload using video.

--- stallmanbot.py
+++ stallmanbot.py
@@ -241,7 +241,7 @@ def Requer(cmd):
     try:
         if re.search("sudo rm -rf /", cmd.text):
             vd = open("%s/sudo_rm_rf.gif" % IMGDIR, "rb")
-            bot.send_document(cmd.chat.id, document = vd)
+            bot.send_video(cmd.chat.id, video = vd)
             return
         bot.reply_to(cmd, "Ah lá... achando que é réquer.")
     except Exception as e:

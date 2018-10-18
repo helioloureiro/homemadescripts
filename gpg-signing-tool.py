71e5f46275beba8947dfb674dedaf8d5ad6ce87d
Helio Loureiro <helio.loureiro@ericsson.com>
2018-10-18 15:40:52 +0200

    Tool started to sign gpg and send signed keys.

---
+++ gpg-signing-tool.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+
+"""
+Handling GPG signing keys, sending automated info to destinations.
+"""
+import sys
+
+class SigningTool:
+    def __init__(self):
+        None
+        self.keys = []
+
+    def get_ids(self, keys = []):
+        """
+        Read key(s) from stdin.
+        """
+        if not keys:
+            if len(sys.argv) < 2:
+                raise("ERROR: wrong usage")
+            else:
+                self.keys = sys.argv[1:]
+        else:
+            self.keys = keys
+
+    def run(self, keys=[]):
+        self.get_ids(keys)
+
+if __name__ == '__main__':
+    s = SigningTool()
+    s.run()

2d7d9aea7d43a635abef360dc49009d2f8adb6bb
Helio Loureiro <helio@loureiro.eng.br>
2022-01-25 11:48:34 +0100

    Improved time handling.
      - includes variables to run as test

--- openwrt-firewall-block-youtube.sh
+++ openwrt-firewall-block-youtube.sh
@@ -2,16 +2,85 @@
 # save it into /usr/lib/scripts/firewall.sh
 # and add into scheduled tasks as
 # */5 * * * * /usr/lib/scripts/firewall.sh
+# requires: iptables-mod-filter bc
+# to run as tests environment: env TEST_ENV=1 DEBUG=1 FAKE_TIME=20:00,Sun sh firewall.sh
 
-hour=`date +%H`
-minute=`date +%M`
+#blocking times
+# HH:MM start, HH:MM stop, weekday
+rules="07:55,08:30,Mon"
+rules="$rules 12:00,18:30,Mon"
+rules="$rules 07:55,08:30,Tue"
+rules="$rules 12:00,18:30,Tue"
+rules="$rules 07:55,08:30,Wed"
+rules="$rules 12:00,18:30,Wed"
+rules="$rules 07:55,08:30,Thu"
+rules="$rules 12:00,18:30,Thu"
+rules="$rules 07:55,08:30,Fri"
+rules="$rules 12:00,18:30,Fri"
 
-echo "hour=$hour"
-echo "minute=$minute"
+die() {
+    echo "$@"
+    exit 1
+}
 
-status_file=/tmp/firewall_status
+debug() {
+    test -n $DEBUG || return
+    echo "$@"
+}
 
-blocked_pattern="youtubei.googleapis.com googlevideo.com ytimg-edge-static.l.google.com i.ytimg.com youtube-ui.l.google.com www.youtube.com googleapis.l.google.com youtubei.googleapis.com video-stats.l.google.com ytimg-edge-static.l.google.com"
+check_dep() {
+    # skip on tests environment
+    test -n $TEST_ENV && return
+
+    dep=$1
+    opkg status $dep | grep -q Installed-Time
+    if [ $? -ne 0 ]; then
+        die "Missing $dep.  Install it running: opkg install $dep"
+    fi
+}
+
+
+
+## Check module installation or fail
+check_dep iptables-mod-filter
+## Check bc or fail
+check_dep bc
+
+if [ ! -n $FAKE_TIME ]; then
+    HOUR=$(date "+%H")
+    MINUTE=$(date "+%M")
+    WEEKDAY=$(date  "+%a")
+else
+    HOUR=$(echo $FAKE_TIME | cut -d, -f1 | cut -d: -f1)
+    MINUTE=$(echo $FAKE_TIME | cut -d, -f1 | cut -d: -f2)
+    WEEKDAY=$(echo $FAKE_TIME | cut -d, -f2)
+fi
+
+echo "Current time: hour=$HOUR minute=$MINUTE (weekday=$WEEKDAY)"
+now=$( echo "$HOUR * 60 + $MINUTE" | bc)
+
+status_file=/tmp/firewall_status.lck
+
+blocked_pattern="youtubei.googleapis.com"
+blocked_pattern="$blocked_pattern googlevideo.com"
+blocked_pattern="$blocked_pattern ytimg-edge-static.l.google.com" 
+blocked_pattern="$blocked_pattern i.ytimg.com" 
+blocked_pattern="$blocked_pattern youtube-ui.l.google.com" 
+blocked_pattern="$blocked_pattern www.youtube.com" 
+blocked_pattern="$blocked_pattern googleapis.l.google.com" 
+blocked_pattern="$blocked_pattern youtubei.googleapis.com" 
+blocked_pattern="$blocked_pattern video-stats.l.google.com" 
+blocked_pattern="$blocked_pattern ytimg-edge-static.l.google.com"
+blocked_pattern="$blocked_pattern instagram.com"
+blocked_pattern="$blocked_pattern facebook.com"
+
+run_cmd() {
+    command="$@"
+    debug "$command"
+    # don't run on test
+    test -n $TEST_ENV && return
+    eval $command
+}
 
 enable_firewall() {
     echo "Enabling firewall"
@@ -22,17 +91,11 @@ enable_firewall() {
             do
                 for blocked in $blocked_pattern
                     do
-                    echo iptables -I $chain $count -p $proto -m string --algo bm --string "$blocked" -j DROP
-                    iptables -I $chain $count -p $proto -m string --algo bm --string "$blocked" -j DROP
-                    count=`expr $count + 1`
+                    cmd="iptables -I $chain $count -p $proto -m string --algo bm --string \"$blocked\" -j DROP"
+                    run_cmd $cmd
+                    count=$(expr $count + 1)
                 done
         done
-        echo iptables -I $chain $count -p udp --sport 443 -j DROP
-        iptables -I $chain $count -p udp --sport 443 -j DROP
-        count=`expr $count + 1`
-        echo iptables -I $chain $count -p udp --dport 443 -j DROP
-        iptables -I $chain $count -p udp --dport 443 -j DROP
-        count=`expr $count + 1`
     done
 }
 
@@ -44,68 +107,143 @@ disable_firewall() {
             do
                 for blocked in $blocked_pattern
                     do
-                    echo iptables -D $chain -p $proto -m string --algo bm --string "$blocked" -j DROP
-                    iptables -D $chain -p $proto -m string --algo bm --string "$blocked" -j DROP
+                    cmd="iptables -D $chain -p $proto -m string --algo bm --string "$blocked" -j DROP"
+                    run_cmd $cmd
                 done
         done
-        echo iptables -D $chain -p udp --sport 443 -j DROP
-        iptables -D $chain -p udp --sport 443 -j DROP
-        echo iptables -D $chain -p udp --dport 443 -j DROP
-        iptables -D $chain -p udp --dport 443 -j DROP
     done
 }
 
+get_status() {
+    if [ ! -f $status_file ];then
+        echo "Not running at this time."
+        exit 0
+    fi
+    status=$(cat $status_file)
+    echo "status: $status"
+}
+
+
+is_running_manual_mode() {
+    if [ ! -f $status_file ];then 
+        echo "no"
+        return
+    fi
+    cat $status_file | grep -q "manual_"
+    if [ $? -eq 0 ]; then
+        echo "yes"
+        return
+    fi
+    echo "no"
+}
+
+convert_minutes() {
+    #format HH:MM
+    timestamp=$1
+    hour=$(echo $timestamp | cut -d: -f1)
+    minute=$(echo $timestamp | cut -d: -f2)
+    echo "$hour * 60 + $minute" | bc
+}
+
+check_running_manual_mode() {
+    status=$(is_running_manual_mode)
+    case $status in
+        yes) die "manual lock in place - use \"reset\" to remove it" ;;
+        no) ;;
+        *) die "uknown error: $status"
+    esac
+}
+
+check_firewall() {
+    # expects [stop|start]
+    action=$1
+
+    check_running_manual_mode
+
+    status=""
+    test -f $status_file && status=$(cat $status_file)
+
+    if [ "$action" = "stop" ] && [ "$status" = "timetable_disabled" ]; then
+        echo "already disabled - nothing to be done"
+        return
+    fi
+
+    if [ "$action" = "stop" ] && [ "$status" != "timetable_disabled" ]; then
+        disable_firewall
+        echo -n "timetable_disabled" > $status_file
+        return
+    fi
+
+    if [ "$action" = "start" ] && [ "status" = "timetable_enabled" ]; then
+        echo "already enabled - nothing to be done"
+        return
+    fi
+
+    if [ "$action" = "start" ] && [ "status" != "timetable_enabled" ]; then
+        enable_firewall
+        echo -n "timetable_enabled" > $status_file
+        return
+    fi
+
+}
+
+
+timetable() {
+
+    check_running_manual_mode
+
+    action="stop"
+    for pattern in $rules
+    do
+        debug "Inspecting rule: $pattern"
+        time_start=$(echo $pattern | cut -d, -f 1)
+        time_stop=$(echo $pattern | cut -d, -f 2)
+        wday=$(echo $pattern | cut -d, -f 3)
+        if [ "$wday" != "$WEEKDAY" ]; then
+            debug "No matching weekday - skipping"
+            continue
+        fi
+
+        time_start=$(convert_minutes $time_start)
+        debug "now: $now < start: $time_start"
+        if  [ $now -lt $time_start ];then
+            debug "Before starting time: $time_start (now: $now) - skipping"
+            continue
+        fi
+
+        time_stop=$(convert_minutes $time_stop)
+        debug "now: $now > stop: $time_stop"
+        if [ $now -eq $time_stop ] || [ $now -gt $time_stop ];then
+            debug "After stopping time: $time_stop (now: $now) - skipping"
+            continue
+        fi
+        debug " * rule activate: $pattern"
+        action="start"
+        break
+    done
+
+    debug "timetable action: $action"
+
+    check_firewall $action
+
+}
+
+
 case $1 in
     start) enable_firewall
-           echo -n "enabled" > $status_file
-           exit 0;;
+        echo -n "manual_enabled" > $status_file
+           ;;
     stop) disable_firewall
-          echo -n "disabled" > $status_file
-          exit 0;;
+          echo -n "manual_disabled" > $status_file
+          ;;
+    restart) $0 stop
+             $0 start
+             ;;
+    status) get_status ;;
+    timetable) timetable ;;
+    reset) $0 stop
+        rm -f $status_file
+        ;;
+    *) echo "Use: $0 [start|stop|status|restart|timetable|reset]"
 esac
 
-# possible status
-# enabled|disabled
-
-# start
-# from 7:55-09:59
-time_status=disabled
-if [[ $hour -ge 7 && $hour -lt 10 ]]; then
-    time_status=enabled
-    if [[ $hour -eq 7 ]]; then
-        if [[ $minute -lt 55 ]]; then
-            time_status=disabled
-        else
-            time_status=enabled
-        fi
-    fi
-fi
-
-# from 12:00-17:59
-if [[ $hour -ge 12 && $hour -lt 18 ]]; then
-    time_status=enabled
-fi
-# from 20:00-21:59
-if [[ $hour -ge 20 && $hour -lt 22 ]]; then
-    time_status=enabled
-fi
-echo "time_status=$time_status"
-
-current_status=
-if [[ -f "$status_file" ]]; then
-    current_status=`cat $status_file`
-fi
-
-if [[ $current_status = $time_status ]]; then
-    echo "nothing to do"
-else
-    if [[ $time_status = "enabled" ]];then
-        echo "loading firewall rules"
-        enable_firewall
-    fi
-    if [[ $time_status = "disabled" ]];then
-        echo "removing firewall rules"
-        disable_firewall
-    fi
-    echo -n $time_status > $status_file
-fi

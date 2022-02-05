b91ffdd4fe382e9710a080ff4684b65cf25190e2
Helio Loureiro <helio@loureiro.eng.br>
2022-02-05 14:52:09 +0100

    Now fetches online the latest content.

--- filter_out_pauta_content.py
+++ filter_out_pauta_content.py
@@ -2,6 +2,64 @@
 
 import argparse
 import requests
+import re
+
+GITHUB = "https://github.com"
+PAUTAS = f"{GITHUB}/helioloureiro/canalunixloadon/tree/master/pautas"
+RAWGITHUB = "https://raw.githubusercontent.com"
+
+
+def curl(url):
+    req = requests.get(url)
+    if req.status_code == 200:
+        return req.text
+    raise Exception(f"Failed to fetch content from {url}")
+
+def getHref(line):
+    """
+    <span class="css-truncate css-truncate-target d-block width-fit"><a class="js-navigation-open Link--primary" title="20220211.md" data-pjax="#repo-content-pjax-container" href="/helioloureiro/canalunixloadon/blob/master/pautas/20220211.md">20220211.md</a></span>
+    """
+    line = re.sub(".*href=\"", "", line)
+    line = re.sub("\">.*", "", line)
+    return line
+
+def unblob(url):
+    return re.sub("/blob", "", url)
+
+class Pautas(object):
+    def __init__(self): None
+
+    def GetLatest(self):
+        links = []
+        pautas = curl(PAUTAS)
+        for line in pautas.splitlines():
+            if not re.search("js-navigation-open Link--primary", line):
+                continue
+            if re.search("template.md", line):
+                continue
+            line = getHref(line)
+            links.append(line)
+        self.latest = sorted(links)[-1]
+        self.latest = unblob(self.latest)
+        self.latestURL = f"{RAWGITHUB}{self.latest}"
+        print("latest  :", self.latestURL)
+        #print("expected: https://raw.githubusercontent.com/helioloureiro/canalunixloadon/master/pautas/20220210.md")
+
+    def GetContentFromRaw(self, url):
+        body = curl(url)
+        content = []
+        for line in body.splitlines():
+            if line.startswith("*"):
+                content.append(line)
+                #print(line)
+        self.lastContent = content
+
+
+    def FetchLatestContent(self):
+        self.GetLatest()
+        self.GetContentFromRaw(self.latestURL)
+        return self.lastContent
+
 
 
 class NewFilter(object):
@@ -20,6 +78,12 @@ class NewFilter(object):
         self.contentFile = args.content
         self.webURL = args.webcontent
 
+        if self.webURL is None or self.webURL == "latest":
+            pauta = Pautas()
+            pauta.GetLatest()
+            self.webURL = pauta.latestURL
+
+
     def Out(self):
         content = []
         with open(self.contentFile) as cFD:
@@ -34,3 +98,4 @@ class NewFilter(object):
 if __name__ == '__main__':
     filter = NewFilter()
     filter.Out()
+

2ba0d3f76cb450c357e4d6c7ed2cb63fc9007449
Helio Loureiro <helio@loureiro.eng.br>
2023-04-16 18:53:23 +0200

    Missed increment variable.

--- render-video-from-gopro-photos.sh
+++ render-video-from-gopro-photos.sh
@@ -7,6 +7,7 @@ do
   new_name="G${serial}.JPG"
   echo "$img => $new_name"
   mv $img $new_name
+  counter=$((counter++))
 done
 
 ffmpeg -hwaccel cuda -hwaccel_output_format cuda -r 1 -i G%04d.JPG -c:v h264_nvenc -b:v 5M -pix_fmt cuda output.mp4

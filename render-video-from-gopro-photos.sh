950cff6b40b5b03d0763cff963ba81119c7312fa
Helio Loureiro <helio@loureiro.eng.br>
2023-04-16 18:33:06 +0200

    Added the script to render GoPro videos.

---
+++ render-video-from-gopro-photos.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+counter=0
+for img in G*.JPG
+do
+  serial=$(printf "%04d" $counter)
+  new_name="G${serial}.JPG"
+  echo "$img => $new_name"
+  mv $img $new_name
+done
+
+ffmpeg -hwaccel cuda -hwaccel_output_format cuda -r 1 -i G%04d.JPG -c:v h264_nvenc -b:v 5M -pix_fmt cuda output.mp4

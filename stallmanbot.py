5df84025ced58b869fa88af9699c22906975e5cd
Helio Loureiro <helio.loureiro@ericsson.com>
2023-05-02 10:59:32 +0200

    More logging info.

--- stallmanbot.py
+++ stallmanbot.py
@@ -1030,16 +1030,19 @@ def UnixLoadOn(obj, cmd):
             #res = os.system(f"git commit -m \"Adding pauta  content at {current_time}\" {pauta_name}")
             res = subprocess.run(["git", "commit", "-m", f"\"Adding pauta  content at {current_time}\"", pauta_name], capture_output=True)
             if res.returncode != 0:
-                return "git commit falhou: " + res.stderr.decode('utf-8')
+                return "git commit falhou: stderr=" + res.stderr.decode('utf-8') + \
+                    ", stdout=" + res.stdout.decode('utf-8')
         else:
             #res = os.system(f"git commit -m \"{message}\" {pauta_name}")
             res = subprocess.run(["git", "commit", "-m", f"\"{message}\"",  pauta_name], capture_output=True)
             if res.returncode != 0:
-                return "git commit falhou: " + res.stderr.decode('utf-8')
+                return "git commit falhou: stderr=" + res.stderr.decode('utf-8') + \
+                    ", stdout=" + res.stdout.decode('utf-8')
         #res = os.system("git push")
         res = subprocess.run(["git", "push"], capture_output=True)
         if res.returncode != 0:
-            return "git push falhou: " + res.stderr.decode('utf-8')
+            return "git push falhou: stderr=" + res.stderr.decode('utf-8') + \
+                    ", stdout=" + res.stdout.decode('utf-8')
         return None
 
     def isAlreadyRegistered(term, text):

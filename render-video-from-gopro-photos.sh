266169a54f90171d8e91fd5a49570e2de8570bc8
Helio Loureiro <helio@loureiro.eng.br>
2023-07-17 18:21:37 +0200

    Improved processing on x86_64 for gopro script
      - Generating 1080p final video.

--- render-video-from-gopro-photos.sh
+++ render-video-from-gopro-photos.sh
@@ -16,7 +16,12 @@ done
 
 case $(uname -s) in
   Linux)
+    echo "Merging images into single video file: output.mp4"
     ffmpeg -hwaccel cuda -hwaccel_output_format cuda -r 1 -i G%06d.JPG -c:v h264_nvenc -b:v 5M -pix_fmt cuda output.mp4
+    echo "Resizing video to 1920x1440: output_1920x1440.mp4"
+    ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i output.mp4 -c:v h264_nvenc -vf scale=1920:1440 -c:a copy output_1920x1440.mp4
+    echo "Cropping file video as 1080p: output_1080p.mp4"
+    ffmpeg -hwaccel cuda -i output_1920x1440.mp4 -c:v h264_nvenc -vf "crop=1920:1080:0:180" output_1080p.mp4
     ;;
   Darwin)
     ffmpeg -r 1 -i G%06d.JPG -c:v h264_videotoolbox -b:v 5M -pix_fmt yuv420p output.mp4
